4e404af4b21bb104d2ccb2357c5c865ea0895fef
lola2607123 <123645230+lola2607123@users.noreply.github.com>
2024-10-19 17:16:29 +0400

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
 st.set_page_config(layout="wide")
+# Load the model
+@st.cache_resource
+def models():
+    mod = YOLO('best.pt') 
+    return mod
+
+
 st.sidebar.image("small.png", use_column_width=True)
 #background picture
 import base64
@@ -29,11 +38,19 @@ with col2:
     st.image("scantitle.png")
 
 
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: rgb(255, 255, 255);text-align:center;
-}
-</style>""", unsafe_allow_html=True)
+with st.container():
+    img = st.file_uploader('Upload your image', type=['jpg', 'png', 'jpeg'])
+    analyse = st.button('Analyze')
 
-b = st.button("Upload your MRI")
+if analyse:
+    if img is not None:
+        img = Image.open(img)
+        st.markdown('Image Visualization')
+        st.image(img)
+        model = models()
+        res = model.predict(img)
+        label = res[0].probs.top5
+        conf = res[0].probs.top5conf
+        conf = conf.tolist()
+        st.write('Detected: ' + str(res[0].names[label[0]].title()))   
+        st.write('Confidence level: ' + str(conf[0]))
