ca6593585f30792e4dc0a83632adf35da6f0c489
lola2607123 <123645230+lola2607123@users.noreply.github.com>
2024-10-25 16:59:10 +0400

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -56,6 +56,8 @@ if analyse:
         st.write('Confidence level: ' + str(conf[0]))
 
     if str(res[0].names[label[0]].title()) != "no_tumor":
-        st.subheader(":white[You have been detected to have a brain tumor, seek immediate consultation with a specialist, such as a neurologist or oncologist. Follow their guidance on further testing to understand the type, size, and location of the tumor. Discuss treatment options, which may include surgery, radiation, or medication, depending on the diagnosis. Support from family and friends can also be crucial during this time, along with considering second opinions for the best care approach.]")
+        st.markdown("You have been detected to have a **brain tumor**, seek immediate consultation with a specialist, such as a neurologist or oncologist. Follow their guidance on further testing to understand size and location of the tumor. Discuss treatment options, which may include surgery, radiation, or medication, depending on the diagnosis. Support from family and friends can also be crucial during this time, along with considering second opinions for the best care approach.")
+        st.markdown('''
+        :red[You have been detected to have a **brain tumor**, seek immediate consultation with a specialist, such as a neurologist or oncologist. Follow their guidance on further testing to understand size and location of the tumor. Discuss treatment options, which may include surgery, radiation, or medication, depending on the diagnosis. Support from family and friends can also be crucial during this time, along with considering second opinions for the best care approach.]''')
     else:
         st.subheader("You're healthy!")
